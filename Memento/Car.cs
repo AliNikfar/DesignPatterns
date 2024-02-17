fed545cc58d47f7d729edd15366d4fafd027fdff
a.nikfarDPK <ali.nikfar2000@gmail.com>
2024-02-17 15:13:27 +0330

    Add And Finished Memento

---
+++ Memento/Car.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    public class CarState
+    {
+        public int Fuel { get; set; }
+        public int Speed { get; set; }
+    }
+
+
+    public class Car
+    {
+        private int _fuel;
+        private int _speed;
+
+        public Car(CarState state)
+        {
+            _fuel = state.Fuel;
+            _speed = state.Speed;
+        }
+
+        public Car()
+        {
+        }
+
+        public void AddFuel( int value)
+        {
+            _fuel += value;
+        }
+        public void IncreaseSpeed()
+        {
+            _speed++;
+        }
+        public void DecreaseSpeed()
+        {
+            _speed--;
+        }
+        public void Kilometers()
+        {
+            _fuel -= 6;
+        }
+        public CarState GetState()
+        {
+            return new CarState()
+            {
+                Fuel = _fuel,
+                Speed = _speed
+            };
+        }
+    }
+}
---
+++ Memento/Program.cs
@@ -0,0 +1,5 @@
+﻿//short Description About Memento: when we have a object  that may  need to continue,Save,... in last state or create a new state
+// in shortly it needs to save current or some states like Undo, Redo , ....
+
+
+// implements in Car.cs
